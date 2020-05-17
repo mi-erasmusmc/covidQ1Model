452c460669e609600cb14d061f6973f1c7e8c7c6
Jenna Reps <jreps@its.jnj.com>
2020-05-17 19:55:49 -0400

    updated
    - Added other models
    - Added plot to show contribution to risk
    - Added shiny dashboard
    
    - TODO: need to add helper html, ohdsi logo and hide plots before button

--- server.R
+++ server.R
@@ -1,46 +1,162 @@
 function(input, output, session) {
 
-    ageCalc <- function(age){
-    if(age < 20)           { value = -7}
-    if(dplyr::between(age,20, 24)){ value = -4}
-    if(dplyr::between(age,25, 29)){ value = -2}
-    if(dplyr::between(age,30, 34)){ value = -2}
-    if(dplyr::between(age,35, 39)){ value = 3}
-    if(dplyr::between(age,40, 44)){ value = 3}
-    if(dplyr::between(age,45, 49)){ value = 6}
-    if(dplyr::between(age,50, 54)){ value = 9}
-    if(dplyr::between(age,55, 59)){ value = 13}
-    if(dplyr::between(age,60, 64)){ value = 15}
-    if(dplyr::between(age,65, 69)){ value = 19}
-    if(dplyr::between(age,70, 74)){ value = 20}
-    if(dplyr::between(age,75, 79)){ value = 23}
-    if(dplyr::between(age,80, 84)){ value = 24}
-    if(dplyr::between(age,85, 89)){ value = 27}
-    if(dplyr::between(age,90, 94)){ value = 25}
+    ageCalc <- function(age, model){
+      
+      if(model == 'Hospitalization'){
+        if(age < 20)           { value = -7}
+        if(dplyr::between(age,20, 24)){ value = -4}
+        if(dplyr::between(age,25, 29)){ value = -2}
+        if(dplyr::between(age,30, 34)){ value = -2}
+        if(dplyr::between(age,35, 39)){ value = 3}
+        if(dplyr::between(age,40, 44)){ value = 3}
+        if(dplyr::between(age,45, 49)){ value = 6}
+        if(dplyr::between(age,50, 54)){ value = 9}
+        if(dplyr::between(age,55, 59)){ value = 13}
+        if(dplyr::between(age,60, 64)){ value = 15}
+        if(dplyr::between(age,65, 69)){ value = 19}
+        if(dplyr::between(age,70, 74)){ value = 20}
+        if(dplyr::between(age,75, 79)){ value = 23}
+        if(dplyr::between(age,80, 84)){ value = 24}
+        if(dplyr::between(age,85, 89)){ value = 27}
+        if(dplyr::between(age,90, 94)){ value = 25}
+        return(value)
+      }
+      if(model == 'Intensive Care'){
+        if(age < 20)           { value = -10}
+        if(dplyr::between(age,20, 24)){ value = -2}
+        if(dplyr::between(age,25, 29)){ value = -1}
+        if(dplyr::between(age,30, 34)){ value = 0}
+        if(dplyr::between(age,35, 39)){ value = 0}
+        if(dplyr::between(age,40, 44)){ value = 3}
+        if(dplyr::between(age,45, 49)){ value = 5}
+        if(dplyr::between(age,50, 54)){ value = 10}
+        if(dplyr::between(age,55, 59)){ value = 12}
+        if(dplyr::between(age,60, 64)){ value = 16}
+        if(dplyr::between(age,65, 69)){ value = 22}
+        if(dplyr::between(age,70, 74)){ value = 21}
+        if(dplyr::between(age,75, 79)){ value = 22}
+        if(dplyr::between(age,80, 84)){ value = 21}
+        if(dplyr::between(age,85, 89)){ value = 25}
+        if(dplyr::between(age,90, 94)){ value = 21}
+        return(value)
+      }
+      
+      if(model == 'Death'){
+        if(age < 20)           { value = -15}
+        if(dplyr::between(age,20, 24)){ value = -8}
+        if(dplyr::between(age,25, 29)){ value = -20}
+        if(dplyr::between(age,30, 34)){ value = -5}
+        if(dplyr::between(age,35, 39)){ value = 0}
+        if(dplyr::between(age,40, 44)){ value = -6}
+        if(dplyr::between(age,45, 49)){ value = 1}
+        if(dplyr::between(age,50, 54)){ value = 15}
+        if(dplyr::between(age,55, 59)){ value = 12}
+        if(dplyr::between(age,60, 64)){ value = 16}
+        if(dplyr::between(age,65, 69)){ value = 27}
+        if(dplyr::between(age,70, 74)){ value = 31}
+        if(dplyr::between(age,75, 79)){ value = 35}
+        if(dplyr::between(age,80, 84)){ value = 40}
+        if(dplyr::between(age,85, 89)){ value = 45}
+        if(dplyr::between(age,90, 94)){ value = 30}
+        return(value)
+      }
+      
+    }
     
-    return(value)
-  }
-          shiny::observeEvent(eventExpr = input$calculate, { 
-            inputData <- data.frame(
-                         Intercept = -50,
-                         age = ageCalc(input$age),
-                         sex = ifelse(input$sex == "Male",3,0),
-                         cancer = input$cancer * 2,
-                         copd = input$copd * 6,
-                         diabetes = input$diabetes * 3,
-                         hd = input$hd * 4,
-                         hl = input$hl * -3,
-                         hypertension = input$hypertension * 3,
-                         kidney = input$kidney * 2
-                         )
-            
-            total <- rowSums(inputData)
-            inputData$risk <- 1/(1+exp(-total/10)) *100
-            inputData
-               
-          output$data <- shiny::renderTable({data = inputData})
-          riskText <- paste0("The patient's risk is: ", round(x = inputData$risk, digits = 1), "%")
-          output$risk <- shiny::renderText(riskText)
-          }) 
-               
+    inputData  <- shiny::reactiveValues(data = NULL, model = NULL)
+    
+    shiny::observeEvent(input$calculate, {
+      
+      inputData$model<- input$model
+      
+      if(input$model == 'Hospitalization'){
+        inputData$data <- data.frame(
+          Intercept = -50,
+          age = ageCalc(input$age, 'Hospitalization'),
+          sex = ifelse(input$sex == "Male",3,0),
+          cancer = input$cancer * 2,
+          copd = input$copd * 6,
+          diabetes = input$diabetes * 3,
+          hd = input$hd * 4,
+          hl = input$hl * -3,
+          hypertension = input$hypertension * 3,
+          kidney = input$kidney * 2
+        )
+      } else if (input$model == 'Intensive Care'){
+        inputData$data <- data.frame(
+          Intercept = -56, # this is wrong!
+          age = ageCalc(input$age, 'Intensive Care'),
+          sex = ifelse(input$sex == "Male",4,0),
+          cancer = input$cancer * 1,
+          copd = input$copd * 6,
+          diabetes = input$diabetes * 4,
+          hd = input$hd * 4,
+          hl = input$hl * -4,
+          hypertension = input$hypertension * 5,
+          kidney = input$kidney * 4
+        )
+      } else{
+        inputData$data <- data.frame(
+          Intercept = -66, # need to add this
+          age = ageCalc(input$age, 'Death'),
+          sex = ifelse(input$sex == "Male",4,0),
+          cancer = input$cancer * 3,
+          copd = input$copd * 4,
+          diabetes = input$diabetes * 2,
+          hd = input$hd * 2,
+          hl = input$hl * -7,
+          hypertension = input$hypertension * 3,
+          kidney = input$kidney * 2
+        )
+      }
+      
+      
+      total <- rowSums(inputData$data)
+      inputData$data$risk <- 1/(1+exp(-total/10)) *100
+    })
+    
+          
+          output$data <- shiny::renderTable({data = inputData$data})
+          riskText <- function(x1, model){
+            if(!is.null(x1)){
+              paste0("The patient's risk of ",model," is: ", round(x = x1, digits = 1), "%")}
+            else{NULL}
+            }
+          output$risk <- shiny::renderText(riskText(inputData$data$risk, inputData$model))
+          
+          
+          
+
+          #contribution of risk
+          output$contributions <- plotly::renderPlotly(plotly::plot_ly(x = as.double(inputData$data)[!names(inputData$data)%in%c('risk','Intercept')], 
+                                                                       y = names(inputData$data)[!names(inputData$data)%in%c('risk','Intercept')], 
+                                                              type = 'bar', orientation = 'h'))
+          
+          
+          
+ 
+          
+          
+          
+          
+          # helpers
+          showInfoBox <- function(title, htmlFileName) {
+            shiny::showModal(shiny::modalDialog(
+              title = title,
+              easyClose = TRUE,
+              footer = NULL,
+              size = "l",
+              shiny::HTML(readChar(htmlFileName, file.info(htmlFileName)$size) )
+            ))
+          }
+          
+          observeEvent(input$RiskInfo, {
+            showInfoBox("Risk", "html/Risk.html")
+          })
+          observeEvent(input$EvidenceInfo, {
+            showInfoBox("Evidence", "html/Evidence.html")
+          })
+          observeEvent(input$AboutInfo, {
+            showInfoBox("About", "html/About.html")
+          })
 }
--- ui.R
+++ ui.R
@@ -1,31 +1,85 @@
-# shinymanager::secure_app(
-  fluidPage(
-  titlePanel("Covid-19 Hospitalisation Risk"),
-  
-  sidebarPanel(
-    shiny::numericInput("age", "Age", 50, min = 18, max = 94),
-    shiny::selectInput("sex","Sex", choices = c("Male", "Female")),
-    shiny::checkboxInput("cancer", "History of Cancer"),
-    shiny::checkboxInput("copd", "History of COPD"),
-    shiny::checkboxInput("diabetes", "History of Diabetes"),
-    shiny::checkboxInput("hd", "History of Heart disease"),
-    shiny::checkboxInput("hl", "History of Hyperlipidemia"),
-    shiny::checkboxInput("hypertension", "History of Hypertension"),
-    shiny::checkboxInput("kidney","History of Kidney Disease"),
-    shiny::actionButton("calculate","Calculate Risk"),
-    
-    hr()
+addInfo <- function(item, infoId) {
+  infoTag <- tags$small(class = "badge pull-right action-button",
+                        style = "padding: 1px 6px 2px 6px; background-color: steelblue;",
+                        type = "button", 
+                        id = infoId,
+                        "i")
+  item$children[[1]]$children <- append(item$children[[1]]$children, list(infoTag))
+  return(item)
+}
 
-  ),
-  
-  mainPanel(
-    shiny::tableOutput("data"),
-    shiny::textOutput("risk"),
-    shinydashboard::box(
-      status = "primary", solidHeader = TRUE,
-      width = 12,
-      shiny::HTML("<br>The evidence for this prediction model can be explored at: <a href=\"https://data.ohdsi.org/Covid19PredictingSimpleModels/\">Link</a>"),
-      shiny::HTML("<br>These models should NOT considered as a replacement for advice from medical professionals.")
-    )
-  )
+ui <- shinydashboard::dashboardPage(skin = 'black',
+                                    
+                                    shinydashboard::dashboardHeader(title = "COVID-19 Predictor", 
+                                                                    
+                                                                    tags$li(div(img(src = 'logo.png',
+                                                                                    title = "OHDSI PLP", height = "40px", width = "40px"),
+                                                                                style = "padding-top:0px; padding-bottom:0px;"),
+                                                                            class = "dropdown")
+                                                                    
+                                                                    
+                                    ), 
+                                    
+                                    shinydashboard::dashboardSidebar(
+                                      shinydashboard::sidebarMenu(
+                                        addInfo(shinydashboard::menuItem("Calculate Risk", tabName = "Risk", icon = shiny::icon("home")), "RiskInfo"),
+                                        addInfo(shinydashboard::menuItem("Evidence", tabName = "Evidence", icon = shiny::icon("table")), "EvidenceInfo"),
+                                        addInfo(shinydashboard::menuItem("About", tabName = "About", icon = shiny::icon("bar-chart")), "AboutInfo")
+                                      )
+                                    ),
+                                    
+                                    shinydashboard::dashboardBody(
+                                      shinydashboard::tabItems(  
+                                        
+                                        shinydashboard::tabItem(tabName = "Risk",                              
+                                                                sidebarPanel(
+                                                                  shiny::selectInput("model","Predict risk of:", choices = c("Hospitalization", "Intensive Care", 'Death')),
+                                                                  shiny::sliderInput("age", "Age:",
+                                                                              min = 18, max = 94,
+                                                                              value = 50),
+                                                                  shiny::selectInput("sex","Sex", choices = c("Male", "Female")),
+                                                                  shiny::checkboxInput("cancer", "History of Cancer"),
+                                                                  shiny::checkboxInput("copd", "History of COPD"),
+                                                                  shiny::checkboxInput("diabetes", "History of Diabetes"),
+                                                                  shiny::checkboxInput("hd", "History of Heart disease"),
+                                                                  shiny::checkboxInput("hl", "History of Hyperlipidemia"),
+                                                                  shiny::checkboxInput("hypertension", "History of Hypertension"),
+                                                                  shiny::checkboxInput("kidney","History of Kidney Disease"),
+                                                                  shiny::actionButton("calculate","Calculate Risk"),
+                                                                  
+                                                                  hr()
+                                                                  
+                                                                ),
+                                                                
+                                                                mainPanel(
+                                                                  shiny::tableOutput("data"),
+                                                                  shiny::textOutput("risk"),
+                                                                  plotly::plotlyOutput("contributions"),
+                                                                  shinydashboard::box(
+                                                                    status = "primary", solidHeader = TRUE,
+                                                                    width = 12,
+                                                                    shiny::HTML("<br>The evidence for this prediction model can be explored at: <a href=\"https://data.ohdsi.org/Covid19PredictingSimpleModels/\">Link</a>"),
+                                                                    shiny::HTML("<br>These models should NOT considered as a replacement for advice from medical professionals.")
+                                                                  )
+                                                                )
+                                        ), # end risk tab,
+                                        
+                                        shinydashboard::tabItem(tabName = "Evidence",
+                                                                shiny::h2("Information"),
+                                                                shiny::p("Add info about the study with paper and shiny result links."),
+                                                                shiny::a("Link 1", href = 'https://', target='_blank'),
+                                                                shiny::a("Link 2", href = 'https://', target='_blank')
+                                        ),
+                                        
+                                        shinydashboard::tabItem(tabName = "About",
+                                                                shiny::h2("About OHDSI"),
+                                                                shiny::p("Add info about OHDSI here with links to website"),
+                                                                shiny::a("Link 1", href = 'https://', target='_blank')
+                                        )
+                                        
+                                      ) # end  tabs
+                                      
+                                      
+                                    )
+                                    
 )
